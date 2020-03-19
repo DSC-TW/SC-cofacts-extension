5fdfe6c6f3ab5f544b2b0b7d186cf8d30e6902ef
littlechintw <littlechintw@gmail.com>
2020-03-19 14:18:40 +0800

    [Featurre] Fix the format

--- functions/main.py
+++ functions/main.py
@@ -1,40 +1,23 @@
 import json
 import requests
-from utility.constants import headers,url
+from utility.constants import url,headers,replies_data
 
 def _get_replies_from_api(title):
     print("title:" + title)
-    data  = {
-        'operationName': 'ListReplies', 
-        'variables': { 
-            'filter': { 
-                'moreLikeThis': { 
-                    'like': 'N/A', 
-                    'minimumShouldMatch': '0' 
-                    } 
-                }, 
-        'orderBy': [{ 
-            '_score': 'DESC' 
-            },{ 
-            'createdAt': 'DESC' 
-            }]
-        }, 
-        'query': 'query ListReplies($filter: ListReplyFilter, $orderBy: [ListReplyOrderBy], $before: String, $after: String) { ListReplies(filter: $filter, orderBy: $orderBy, before: $before, after: $after, first: 10) { edges { node {  ...ReplyItem  __typename } cursor __typename } __typename } }  fragment ReplyItem on Reply { id text type createdAt user { id name __typename } articleReplies(status: NORMAL) { articleId replyId __typename } __typename } ' 
-    }
-    data['variables']['filter']['moreLikeThis']['like'] = title
-    data = json.dumps(data)
+    replies_data['variables']['filter']['moreLikeThis']['like'] = title
+    data = json.dumps(replies_data)
     try:
-        r = requests.post(url, headers=headers, data=data)
-        r.encoding = 'utf8'
-        print(r.status_code)
+        response = requests.post(url, headers=headers, data=data)
+        response.encoding = 'utf8'
+        print(response.status_code)
     except:
         print('\033[33m Error! \033[0m')
-        return {'status': r.status_code}
+        return {'status': response.status_code}
     else:
         print('\033[33m Success! \033[0m')
-        json_data = json.loads(r.text)
+        json_data = json.loads(response.text)
         json_data = json_data['data']['ListReplies']['edges']
-        return {'status': r.status_code, 'content':_replies_data_process(json_data)}
+        return {'status': response.status_code, 'content':_replies_data_process(json_data)}
 
 def _replies_data_process(json_data):
     data = json_data
@@ -50,10 +33,7 @@ def _replies_data_process(json_data):
 def get_replies(request):
     from flask import abort
 
-    if request.method == 'GET':
-        output = 'You can try using POST method!'
-        return output
-    elif request.method == 'POST':
+    if request.method == 'POST':
         my_json = request.data.decode('utf8').replace("'", '"')
         data = json.loads(my_json)
         return str(_get_replies_from_api(data['title']))
--- functions/utility/constants.py
+++ functions/utility/constants.py
@@ -1,3 +1,5 @@
+url = "https://cofacts-api.g0v.tw/graphql"
+
 headers = {
     "Connection": "keep-alive",
     "accept": "*/*",
@@ -13,4 +15,20 @@ headers = {
     "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8"
 }
 
-url = "https://cofacts-api.g0v.tw/graphql"
+replies_data  = {
+        'operationName': 'ListReplies', 
+        'variables': { 
+            'filter': { 
+                'moreLikeThis': { 
+                    'like': 'N/A', 
+                    'minimumShouldMatch': '0' 
+                    } 
+                }, 
+        'orderBy': [{ 
+            '_score': 'DESC' 
+            },{ 
+            'createdAt': 'DESC' 
+            }]
+        }, 
+        'query': 'query ListReplies($filter: ListReplyFilter, $orderBy: [ListReplyOrderBy], $before: String, $after: String) { ListReplies(filter: $filter, orderBy: $orderBy, before: $before, after: $after, first: 10) { edges { node {  ...ReplyItem  __typename } cursor __typename } __typename } }  fragment ReplyItem on Reply { id text type createdAt user { id name __typename } articleReplies(status: NORMAL) { articleId replyId __typename } __typename } ' 
+    }
