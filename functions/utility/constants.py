de381c5e20d04cbe288a4841245fa282a8086985
littlechintw <littlechintw@gmail.com>
2020-03-19 13:53:07 +0800

    [Feature] Add const

---
+++ functions/utility/constants.py
@@ -0,0 +1,14 @@
+headers = {
+    "Connection": "keep-alive",
+    "accept": "*/*",
+    "x-app-id": "RUMORS_SITE",
+    "DNT": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.62 Safari/537.36",
+    "content-type": "application/json",
+    "Origin": "https://cofacts.g0v.tw",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://cofacts.g0v.tw/",
+    "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8"
+}
