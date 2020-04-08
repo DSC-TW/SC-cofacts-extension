2004b98ab26794bad2b63944438c3d7205decdf2
littlechintw <littlechintw@gmail.com>
2020-04-08 14:28:44 +0800

    [ Fix ] Use Flask

--- functions/main.py
+++ functions/main.py
@@ -1,6 +1,9 @@
 import json
+from flask import Flask, request
 import requests
 from utility.constants import url,headers,replies_data
+
+app = Flask(__name__)
 
 def _get_replies_from_api(title):
     print("title:" + title)
@@ -30,15 +33,19 @@ def _replies_data_process(json_data):
         output.append({"text": text, "text_type": text_type, "creator": creator, "createdTime": createdTime})
     return output
 
-def get_replies(request):
-    from flask import abort
-
-    if request.method == 'POST':
-        my_json = request.data.decode('utf8').replace("'", '"')
-        data = json.loads(my_json)
-        return str(_get_replies_from_api(data['title']))
+@app.route('/', methods=['POST'])
+def get_replies():
+    try:
+        data = request.get_json()
+    except:
+        content = {
+            "code": 406,
+            "status": "NOT_ACCEPTABLE",
+            "message": "No given data",
+        }
+        return content, 406
     else:
-        return abort(403)
+        return str(_get_replies_from_api(data['title'])), 200
 
 if __name__ == "__main__":
-    get_replies(request)
+    app.run(host='127.0.0.1', port=4040, debug=True)
