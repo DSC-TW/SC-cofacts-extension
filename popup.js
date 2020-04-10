b0686e4e7e5e3c3eba2fe528199b389b7ed6aead
alanhc <alanhc.tseng1999@gmail.com>
2020-04-11 02:39:53 +0800

    add response to popup.html

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
+/*
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
     document.getElementById("output").innerHTML = selection[0];
 });
+*/
+chrome.storage.sync.get('msg',(response)=>{
+    document.getElementById("output").innerHTML =response.msg
+});
+
+
