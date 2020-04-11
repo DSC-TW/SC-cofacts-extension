8f977fc4848102547775a638ca31f2df24e119e2
alanhc <alanhc.tseng1999@gmail.com>
2020-04-11 22:35:47 +0800

    fix popup

--- popup.js
+++ popup.js
@@ -5,6 +5,11 @@ chrome.tabs.executeScript( {
     document.getElementById("output").innerHTML = selection[0];
 });
 */
+/*
+setInterval(function() { 
+    //如果沒有點開就有下面塞到這
+},100);
+*/
 chrome.storage.sync.get('msg',(response)=>{
     document.getElementById("output").innerHTML =response.msg
 });
